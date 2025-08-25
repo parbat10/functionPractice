3ba0a62d78b6f88f624a380fef536883dd3ef799
parbat10 <parbatlimbu139@gmail.com>
2025-08-25 15:38:02 +0545

    practicing

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-let add = function addition(){
+let add = function (){
     console.log(2+3);
-}
-setTimeout(add,1000);
-setTimeout(add,2000);
+};
+add();
+add();
 
 function runTwice(fun){
-    fun;
-    fun;
+    fun();
+    fun();
 }
 runTwice( function(){
-    console.log(add);
+    console.log('12b');
 });
 runTwice(add);
+
+
+
+
+
+
+function updateButton(){
+    const button = document.querySelector('.js-button');
+
+    setTimeout(function(){
+        button.innerHTML='loading....';
+    },1000);
+    setTimeout(function(){
+        button.innerHTML='finished!';
+    },2000);
+    setTimeout(function(){
+        button.innerHTML='click'
+    },7000);
+}
+
+
+
+    let timeoutId ;
+
+function addToCart(){
+    const paragraph = document.querySelector('.js-paragraph');
+    paragraph.innerHTML = 'added';
+
+    clearTimeout(timeoutId);
+
+    timeoutId =setTimeout(function(){
+        paragraph.innerHTML='';
+    },2000);
+}
+
+
+
+let message = 0 ;
+function addButton(){
+    message++;
+};
+
+function removeButton(){
+    message--;
+};
+
+
+setInterval(function() {
+    if(document.title === 'App'){
+        document.title = `(${message}) messages`
+    }else{
+        document.title = 'App'
+    }
+    
+}, 1000);
